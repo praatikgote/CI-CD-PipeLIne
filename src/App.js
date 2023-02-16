058fd98070416af99381bde52da281ce94edce95
praatikgote <pratikgote69@gmail.com>
2023-02-16 23:45:47 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> YoYo :-)
+          Edit <code>src/App.js</code> Pratik Gote CI/CD :-)
         </p>
         <a
           className="App-link"
