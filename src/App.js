dfee2e06e41208db466952d5022a0afc6d056067
praatikgote <pratikgote69@gmail.com>
2023-02-16 23:53:19 +0530

    star Chenges

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Pratik Gote CI/CD :-)
+          Edit <code>src/App.js</code>************************** Pratik Gote ************************ :-)
         </p>
         <a
           className="App-link"
