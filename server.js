918768bfa771c48bf803457d539f012d61918dcf
praatikgote <pratikgote69@gmail.com>
2023-02-19 00:33:25 +0530

    port

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const app = express();
 app.use(express.static(path.join(__dirname, "./build")));
 app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "./build/index.html")));
 
-const PORT = process.env.PORT || 3009;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
