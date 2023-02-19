0fefdeeb990df2246a0e02437dcb3ec3248f9751
praatikgote <pratikgote69@gmail.com>
2023-02-19 23:41:46 +0530

    build

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>pratik Gote 789789:-)
+          Edit <code>src/App.js</code>pratik Goteeeeeeee:-)
         </p>
         <a
           className="App-link"
