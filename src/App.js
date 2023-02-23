92c43bc1ad066faeed28b1ea43d5aa8e942be3f5
praatikgote <pratikgote69@gmail.com>
2023-02-23 13:43:01 +0530

    change to different name

--- src/App.js
+++ src/App.js
@@ -7,10 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>pratik Gote Pranav :-)
+          Edit <code>src/App.js</code>pratik Gote Pranav Anil Sunil Nitin Annapurna Chandrashekhar :-)
         </p>
         <a
-          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
